c38cae1311b1f86bd1f77f408207615673cb8e07
Rohini <rohinikanagaraj18@gmail.com>
2026-04-25 16:54:28 -0400

    changed code

--- FastAPI/src/rag.py
+++ FastAPI/src/rag.py
@@ -1,49 +1,45 @@
 from langchain_core.prompts.chat import ChatPromptTemplate 
-from langchain_core.runnables import RunnablePassthrough,RunnableParallel
+from langchain_core.runnables import RunnablePassthrough, RunnableParallel
 from decouple import config
 from operator import itemgetter
 from langchain_openai import ChatOpenAI
-from Qdrant import vectorstore
+from Qdrant import get_vectorstore
 
-model=ChatOpenAI(
-    model="gpt-5.4",
+model = ChatOpenAI(
+    model="gpt-4o",
     temperature=0.7,
     openai_api_key=config("OPENAI_API_KEY"),
 )
 
-prompt_template= """
- Answer the question based on the context in a concise manner only from the website
+prompt_template = """
+Answer the question based on the context in a concise manner only from the website
 and using bullet points where applicable
-context :{context}
-Question:{question}
+context: {context}
+Question: {question}
 Response:
 """
-prompt=ChatPromptTemplate.from_template(prompt_template)
-retriever = vectorstore.as_retriever()
+prompt = ChatPromptTemplate.from_template(prompt_template)
 
 def createchain():
+    vectorstore = get_vectorstore()
+    retriever = vectorstore.as_retriever()
     chain = (
         {
-            "context":retriever.with_config(top_k=4),
+            "context": retriever.with_config(top_k=4),
             "question": RunnablePassthrough()
-
-        }|RunnableParallel({
-            "response": prompt|model,
+        } | RunnableParallel({
+            "response": prompt | model,
             "context": itemgetter("context")
-          }
-               )
-        
+        })
     )
     return chain
 
-def return_answer(question:str):
-    chain= createchain()
-    response= chain.invoke(question)
-    answer=response["response"].content
-    context=response["context"]
-    return  {
-        "answer":answer,
+def return_answer(question: str):
+    chain = createchain()
+    response = chain.invoke(question)
+    answer = response["response"].content
+    context = response["context"]
+    return {
+        "answer": answer,
         "context": context
     }
-    
-    

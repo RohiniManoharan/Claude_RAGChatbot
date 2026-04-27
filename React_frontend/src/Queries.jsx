cc34260552852f811f55c1c2d17b7a73bf01fc99
Rohini <rohinikanagaraj18@gmail.com>
2026-04-27 18:23:32 -0400

    Added vite Api url

--- React_frontend/src/Queries.jsx
+++ React_frontend/src/Queries.jsx
@@ -1,7 +1,7 @@
 
 import { useState } from 'react';
 import axios from 'axios';
-const api = axios.create({baseURL:'http://localhost:8000'})
+const api = axios.create({baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000'})
 function Queries(){
     const [question,setQuestion]=useState('');
     const [answer,setAnswer]= useState('');
